2b1339fb8612d97cc324ce9cba37d6254b3036f1
Bruno Santos <levi@MacBook-Air-de-Bruno.local>
2023-06-06 17:28:45 -0300

    unset

--- SRCS/builtins/my_unset.c
+++ SRCS/builtins/my_unset.c
@@ -6,13 +6,37 @@
 /*   By: bde-seic <bde-seic@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:12:09 by bde-seic          #+#    #+#             */
-/*   Updated: 2023/04/26 19:24:29 by bde-seic         ###   ########.fr       */
+/*   Updated: 2023/06/06 17:24:24 by bde-seic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// int	my_unset(char *quoted_line)
-// {
-	
-// }
+int	my_unset(char **flags)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**new_env;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (!ft_strncmp(meta()->envp[i], flags[1], ft_strlen(flags[1])))
+		i++;
+	if (i < count_strings(meta()->envp))
+	{
+		new_env = malloc(sizeof(char **) * count_strings(meta()->envp));
+		while (meta()->envp[j])
+		{
+			if (j == i)
+				j++;
+			strcpy(new_env[k], meta()->envp[j]); //alterar
+			k++;
+			j++;
+		}
+		new_env[k] = 0;
+		meta()->envp = new_env;
+	}
+	return (1); //ha de ser preciso fazer free disto
+}
